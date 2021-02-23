df2dbfd2c1f65ab4fc958a5a45cbf747467b601f
A01705746 <65366693+A01705746@users.noreply.github.com>
2021-02-23 02:29:53 -0600

    terminar secuencia de botones

--- js/zelda2.js
+++ js/zelda2.js
@@ -9,7 +9,13 @@ function dar_espada(){
     boton.innerHTML = "¡A luchar!";
     boton.onclick = () => {
         console.log("A luchar!!!"); // Declara la función sin parámetros
-        let imagen = document.getElementById("link_luchando");
-        imagen.innerHTML = '<img alt="Gif retro animado de Link luchando" src="https://steemitimages.com/p/2gsjgna1uruvUuS7ndh9YqVwYGPLVszbFLwwpAYXZoZbh6aBvoQj9NjW3onDQDqG6HHpeWNewo4T2z9RXPgLvmtFM43hgQp5anwL7oY84nBZhwY81s?format=match&mode=fit">';
+        let resultado = document.getElementById("link_luchando");
+        resultado.innerHTML = '<img alt="Gif retro animado de Link luchando" src="https://steemitimages.com/p/2gsjgna1uruvUuS7ndh9YqVwYGPLVszbFLwwpAYXZoZbh6aBvoQj9NjW3onDQDqG6HHpeWNewo4T2z9RXPgLvmtFM43hgQp5anwL7oY84nBZhwY81s?format=match&mode=fit">';
+        boton.innerHTML = "Dejar de luchar";
+        boton.onclick = () => {
+            resultado.innerHTML = "<br><br><h4>Link derrotó a " + Math.floor(Math.random() * 10) + " monstruos</h4>";
+            let div = document.getElementById("boton");
+            div.innerHTML = "";
+        }
     }
 }
