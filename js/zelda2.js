c06188b4ee26cde38fed148a9d1af4a4c63c69b4
A01705746 <65366693+A01705746@users.noreply.github.com>
2021-02-23 02:14:34 -0600

    Agregar una imágen de manera dinámica al HTML

--- js/zelda2.js
+++ js/zelda2.js
@@ -9,5 +9,7 @@ function dar_espada(){
     boton.innerHTML = "¡A luchar!";
     boton.onclick = () => {
         console.log("A luchar!!!"); // Declara la función sin parámetros
+        let imagen = document.getElementById("link_luchando");
+        imagen.innerHTML = '<img alt="Gif retro animado de Link luchando" src="https://steemitimages.com/p/2gsjgna1uruvUuS7ndh9YqVwYGPLVszbFLwwpAYXZoZbh6aBvoQj9NjW3onDQDqG6HHpeWNewo4T2z9RXPgLvmtFM43hgQp5anwL7oY84nBZhwY81s?format=match&mode=fit">';
     }
 }
