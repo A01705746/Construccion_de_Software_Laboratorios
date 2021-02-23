eeab4cd36fb1e6485825d26d35e8deeacb8c7318
A01705746 <65366693+A01705746@users.noreply.github.com>
2021-02-23 01:55:32 -0600

    Modificar los eventos de un elemento HTML de manera dinámica

--- js/zelda2.js
+++ js/zelda2.js
@@ -1,8 +1,15 @@
 // DOM: Document Object Model
 
+function luchar(){
+    console.log("A luchar!!!");
+}
 
 function dar_espada(){
     console.log("Darle una espada a link");
     let espada = document.getElementById("espada");
     espada.innerHTML = "<li>Espada</li>";
+
+    let boton = document.getElementById("acciones_link")
+    boton.innerHTML = "¡A luchar!";
+    boton.onclick = luchar;
 }
