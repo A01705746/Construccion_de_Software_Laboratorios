92feb21584b812823e676a1e95f8335d61ddf27a
A01705746 <65366693+A01705746@users.noreply.github.com>
2021-04-05 18:53:51 -0500

    Mi primer servidor web

--- Sesiones index/app.js
+++ Sesiones index/app.js
@@ -2,14 +2,27 @@ console.log("Hola desde Node");
 
 // fs: filesystem
 const filesystem = require('fs');
-
 filesystem.writeFileSync('Hola.txt', 'Hola desde Node');
 
+// imprimir el arreglo en orden con código asínccrono
 const arreglo = [5000, 60, 90, 100, 10, 20, 0, 120, 2000, 340, 1000, 50];
-
 for (let item of arreglo) {
     setTimeout(() => {
         console.log(item);
     }, item);
-    
 }
+
+const http = require('http');
+
+const server = http.createServer( (request, response) => {
+    console.log("Hola desde el servidor");
+    console.log(request.url);
+    response.setHeader('Content-Type', 'text/html');
+    response.write("<html>");
+    response.write("<head><title>Servidor Node</title></head>");
+    response.write("<body><h1>Hola desde el servidor</h1></body>");
+    response.write("</html>");
+    response.end();
+});
+
+server.listen(3000)
