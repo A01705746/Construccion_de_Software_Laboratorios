d0cc1fedc5f1a77fbcad07404f4b264d820a3e80
A01705746 <65366693+A01705746@users.noreply.github.com>
2021-04-07 18:04:47 -0500

    Manejo de rutas

--- app.js
+++ app.js
@@ -7,12 +7,32 @@ console.log('Genial!');
 // {nombre_atributo: valor}
 
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 
 //Middleware
+app.use(bodyParser.urlencoded({extended: false}));
+
 app.use((request, response, next) => {
     console.log('Middleware!');
     next(); //Le permite a la petición avanzar hacia el siguiente middleware
+});
+
+app.get('/campeones/nuevo-campeon', (request, response, next) => {
+    response.send('<h1>Nuevo Campeon</h1><body><h1>Agrega un campeón</h1><form action="nuevo-campeon" method="POST"><input type="text" name="nombre"><input type="submit" value="Guardar campeón"></form></body>'); 
+});
+
+app.post('/campeones/nuevo-campeon', (request, response, next) => {
+    console.log(request.body.nombre)
+    response.send('<h1>Campeon guardado</h1>');
+});
+
+app.use('/campeones', (request, response, next) => {
+    response.send('<h1>Campeones</h1>');
+});
+
+app.use('/', (request, response, next) => {
+    response.send('<h1>¡Hola mundo!</h1>'); 
 });
 
 app.use((request, response, next) => {
