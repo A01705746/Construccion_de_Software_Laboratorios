b6f53580b64860e823db0666e930aafd4d052dda
A01705746 <65366693+A01705746@users.noreply.github.com>
2021-04-07 17:04:30 -0500

    Primer Middleware

--- app.js
+++ app.js
@@ -5,3 +5,19 @@ console.log('Genial!');
 // ¿Qué es json?
 // json: javascript object notation
 // {nombre_atributo: valor}
+
+const express = require('express');
+const app = express();
+
+//Middleware
+app.use((request, response, next) => {
+    console.log('Middleware!');
+    next(); //Le permite a la petición avanzar hacia el siguiente middleware
+});
+
+app.use((request, response, next) => {
+    console.log('Otro middleware!');
+    response.send('¡Hola mundo!'); //Manda la respuesta
+});
+
+app.listen(3000);
