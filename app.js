22e1b09070c162881e925b9ef218de56f7b44a5f
A01705746 <65366693+A01705746@users.noreply.github.com>
2021-04-07 16:12:24 -0500

    App básica en npm

---
+++ app.js
@@ -0,0 +1,7 @@
+console.log('hola, mundo!');
+console.log('Ya no tuve que detener el servidor');
+console.log('Genial!');
+
+// ¿Qué es json?
+// json: javascript object notation
+// {nombre_atributo: valor}
