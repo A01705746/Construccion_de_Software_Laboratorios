0b2c5209544f833319fe5a4d298afec81b7bc018
A01705746 <65366693+A01705746@users.noreply.github.com>
2021-04-15 17:32:46 -0500

    Refactorizar rutas personajes con controladores

--- Laboratorio 11/controllers/campeones_controller.js
+++ Laboratorio 11/controllers/campeones_controller.js
@@ -1,6 +1,24 @@
 const personajes = ["Ahri", "Akali", "Evelynn", "Sona"];
 const nuevos_personajes = [];
 
+exports.getNuevoCampeon = (request, response, next) => {
+    response.render('nuevo-campeon', {
+        titulo: 'Nuevo Campeón'
+    });
+};
+
+exports.postNuevoCampeon = (request, response, next) => {
+    console.log(request.body.nombre_campeon);
+    personajes.push(request.body.nombre_campeon);
+    nuevos_personajes.push(request.body.nombre_campeon);
+
+    console.log("Campeones guardados en Nuevos Campeones.txt");
+    const filesystem = require('fs');
+    filesystem.writeFileSync('Nuevos Campeones.txt', nuevos_personajes.toString());
+
+    response.redirect('/campeones');
+};
+
 exports.get = (request, response, next) => {
     response.render('campeones', {
         lista_personajes: personajes,
--- Laboratorio 11/routes/campeones.js
+++ Laboratorio 11/routes/campeones.js
@@ -6,26 +6,9 @@ const path = require('path');
 
 const campeonesController = require('../controllers/campeones_controller');
 
-const personajes = ["Ahri", "Akali", "Evelynn", "Sona"];
-const nuevos_personajes = [];
+router.get('/nuevo-campeon', campeonesController.getNuevoCampeon);
 
-router.get('/nuevo-campeon', (request, response, next) => {
-    response.render('nuevo-campeon', {
-        titulo: 'Nuevo Campeón'
-    });
-});
-
-router.post('/nuevo-campeon', (request, response, next) => {
-    console.log(request.body.nombre_campeon);
-    personajes.push(request.body.nombre_campeon);
-    nuevos_personajes.push(request.body.nombre_campeon);
-
-    console.log("Campeones guardados en Nuevos Campeones.txt");
-    const filesystem = require('fs');
-    filesystem.writeFileSync('Nuevos Campeones.txt', nuevos_personajes.toString());
-
-    response.redirect('/campeones');
-});
+router.post('/nuevo-campeon', campeonesController.postNuevoCampeon);
 
 router.use('/', campeonesController.get);
 
