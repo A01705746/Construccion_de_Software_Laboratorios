d38d5d8c166163fe2816f263f678787537ea1335
A01705746 <65366693+A01705746@users.noreply.github.com>
2021-04-18 14:54:44 -0500

    Agregar cookie

--- Laboratorio 11/controllers/campeones_controller.js
+++ Laboratorio 11/controllers/campeones_controller.js
@@ -10,10 +10,17 @@ exports.postNuevoCampeon = (request, response, next) => {
     console.log(request.body.nombre_campeon);
     const nuevo_campeon = new Campeon(request.body.nombre_campeon, request.body.imagen_campeon);
     nuevo_campeon.save();
+
+    response.setHeader('Set-Cookie', ['ultimo_campeon='+ nuevo_campeon.nombre]);
+
     response.redirect('/campeones');
 };
 
 exports.get = (request, response, next) => {
+
+    console.log('Cookie: ' + request.get('Cookie'));
+    console.log(request.get('Cookie').split(';')[1].trim().split('=')[1]);
+
     response.render('campeones', {
         lista_personajes: Campeon.fetchAll(),
         titulo: 'Campeones'
